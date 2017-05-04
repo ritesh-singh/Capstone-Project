4f0bc2a3e62406955d159a6d34b2109cc834763e
RITESH KUMAR SINGH <ritesh.singh@bookmyshow.com>
2017-05-04 18:34:30 +0530

    Removed comments

--- Capstone_stage2/app/src/main/java/com/example/riteshkumarsingh/capstone_stage2/data/sync/JobSchedulerService.java
+++ Capstone_stage2/app/src/main/java/com/example/riteshkumarsingh/capstone_stage2/data/sync/JobSchedulerService.java
@@ -31,13 +31,6 @@ import rx.subscriptions.CompositeSubscription;
 /**
  * Created by riteshkumarsingh on 04/05/17.
  */
-
-
-/**
- *  Job service that will be used by the Job Scheduler,
- *  when scheduled.
- */
-
 public class JobSchedulerService extends JobService {
 
     private DatabaseReference mDataBase;
@@ -138,19 +131,7 @@ public class JobSchedulerService extends JobService {
                 });
     }
 
-
-    /**
-     * This function is responsible for syncing all data to
-     * firebase server.
-     */
     private void syncDataToServer(JobParameters jobParameters){
-        // TODO - call this, when the task is completed.
-
-        // It takes 2 params
-        // 1.jobParameters - the current job, so that it knows which wakelock can be released
-        // 2.boolean - indicating whether you’d like to reschedule the job.
-        // If you pass in true, this will kick off the JobScheduler’s exponential backoff logic for you
-//        jobFinished(jobParameters,!success);
 
         Observable<Movies> popularMovieObservable =  getPopularMovies
                 .getMovies(Utils.getMovieOptions("1"))
@@ -208,29 +189,12 @@ public class JobSchedulerService extends JobService {
     }
 
 
-    /**
-     * Called by system - when it time to execute
-     *
-     * return false - let system know all work has been completed (for simple task)
-     * return true - for complicated task
-     *
-     * Job service run on main thread by default.
-     */
     @Override
     public boolean onStartJob(JobParameters jobParameters) {
         syncDataToServer(jobParameters);
-        return true; // return true - tell system long running task is running, it should hold on to wakelock for little longer.
+        return true;
     }
 
-
-    /**
-     * called by the system if the job is cancelled before being finished.
-     * such as when the device has been unplugged or if WiFi is no longer available.
-     * So use this method for any safety checks and clean up you may need to do in response to a half-finished job
-     *
-     * Then, return true if you’d like the system to reschedule the job,
-     * or false if it doesn’t matter and the system will drop this job
-     */
     @Override
     public boolean onStopJob(JobParameters jobParameters) {
         RxUtils.unSubscribe(mSubscription);
